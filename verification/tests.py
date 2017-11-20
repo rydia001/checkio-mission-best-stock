92c7f4239f087589d7d7bff10f42dc85fac0bd22
oleksandrliabakh <oleksandrliabakh@Oleksandrs-MacBook.local>
2017-11-20 17:42:16 +0200

    initial commit

---
+++ verification/tests.py
@@ -0,0 +1,31 @@
+
+
+TESTS = {
+    "Basics": [
+        {
+            "input": {
+                'CAC': 10.0,
+                'ATX': 390.2,
+                'WIG': 1.2
+            },
+            "answer": 'ATX'
+        },
+        {
+            "input": {
+                'CAC': 91.1,
+                'ATX': 1.01,
+                'TASI': 120.9
+            },
+            "answer": 'TASI'
+        }
+    ],
+    "Extra": [
+        {
+            "input": {
+                'ATX': 230.0,
+                'GOOG': 100000.01
+            },
+            "answer": 'GOOG'
+        }
+    ]
+}
